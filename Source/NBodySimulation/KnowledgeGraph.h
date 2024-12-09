c4d567f5bc3a2ed73e87e1f325ea8f248396c0f5
Your Name <you@example.com>
2024-12-09 14:00:24 +0800

    a

--- Source/NBodySimulation/KnowledgeGraph.h
+++ Source/NBodySimulation/KnowledgeGraph.h
@@ -28,9 +28,9 @@ public:
 	float strength;
 	float distance;
 
-	
+
 	AKnowledgeEdge* edge;
-	
+
 	Link(int32 source, int32 target, AKnowledgeEdge* edge)
 	{
 		this->source = source;
@@ -44,18 +44,15 @@ public:
 		this->target = target;
 	}
 
-	
 
 	Link()
 	{
-		
 	}
 };
 
 class Node
 {
 public:
-
 	int id;
 	AKnowledgeNode* node;
 
@@ -69,11 +66,7 @@ public:
 		this->id = id;
 		this->node = node;
 	}
-	
 };
-
-
-
 
 
 UCLASS()
@@ -84,26 +77,26 @@ public:
 	bool use_shaders = false;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	int32 shaderdebug = 0;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool use_Jason = false;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	int32 JSONFileIndex = 0;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
-	bool cpu_linkc=true;
+	bool cpu_linkc = true;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	// Calculate many body force or not. 
 	bool cpu_manybody = true;
-	
+
 	GENERATED_BODY()
 	AKnowledgeGraph();
 	virtual ~AKnowledgeGraph() override;
-	
-	bool prechecksucceeded=true;
-	
+
+	bool prechecksucceeded = true;
+
 	// Boolean for signaling whether this round of computation from GPU is valid. 
 	bool GPUvalid = false;
 	// Integer ID to original string ID. 
@@ -111,10 +104,10 @@ public:
 	// Original string ID to integer ID.
 	TMap<FString, int32> string_to_id;
 	TArray<Node> all_nodes2;
-	
+
 	TArray<FVector> nodePositions;
 	TArray<FVector> nodeVelocities;
-	
+
 	TArray<Link> all_links2;
 
 	OctreeNode* OctreeData2;
@@ -123,27 +116,27 @@ public:
 	TArray<double> ElapsedTimes;
 
 	FNBodySimParameters SimParameters;
-	
+
 	UPROPERTY()
 	TArray<FTransform> BodyTransforms;
 
 	UPROPERTY(VisibleAnywhere, Instanced)
 	TObjectPtr<UInstancedStaticMeshComponent> InstancedStaticMeshComponent;
 
-	TMap<int32, FString> fileIndexToPath= {
+	TMap<int32, FString> fileIndexToPath = {
 		{0, "statered.json"},
 		{1, "state - 2024-06-18T223257.374.json"},
 		{2, "state777777777.json"},
 	};
 
-	
+
 	void prepare();
 	void Updatmeterinshader(float DeltaTime);
 	bool Earlyexit(bool log);
 	void CPUcalculate();
 	void Updatepositionarray(bool log);
 	void UpdateAlpha();
-	
+
 
 	void generateGraph();
 	bool Generateactorandattach(AKnowledgeNode*& kn);
@@ -157,15 +150,15 @@ public:
 	void initializeNodePosition();
 
 	void initializeNodePosition_Individual(int index
-		);
+	);
 
 	void CalculateBiasstrengthOflinks();
-	
+
 	void ApplyForces();
 	void calculate_link_force_and_update_velocity();
 	void calculate_charge_force_and_update_velocity();
 	void calculate_centre_force_and_update_position();
-	
+
 	void update_Node_world_position_according_to_position_array();
 	void update_position_array_according_to_velocity_array();
 	void update_link_position();
@@ -175,33 +168,30 @@ public:
 
 	/////////////////////////////////////////////////////////////////////////////////
 
-	TArray<int> LinkOffsets;  // Holds the offset for each body
-	TArray<int> LinkCounts;   // Holds the count of links for each body
-	TArray<int> LinkIndices;  // Flat array containing all links
-	TArray<float> LinkStrengths;  // Holds the strength of each link
-	TArray<float> LinkBiases;     // Holds the bias of each link
-	TArray<int> Linkinout;  
-
-
-
+	TArray<int> LinkOffsets; // Holds the offset for each body
+	TArray<int> LinkCounts; // Holds the count of links for each body
+	TArray<int> LinkIndices; // Flat array containing all links
+	TArray<float> LinkStrengths; // Holds the strength of each link
+	TArray<float> LinkBiases; // Holds the bias of each link
+	TArray<int> Linkinout;
 
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
+
 	// Turn on logging or not. 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool use_logging = false;
-	
+
 
 	// Every node will be Initialize with position 0. 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool initialize_with_zero_position = false;
-	
+
 
 	//Node setting////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// 1////////////////////
-	
+
 	// Use instance static mesh or not
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool use_instance_static_mesh_fornode = false;
@@ -210,22 +200,22 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	float instance_static_mesh_size = 0.3f;
 
-	
+
 	// 2////////////////////
-	
+
 	// Use a lot of actor Or not
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool use_actor_fornode = true;
-	
+
 	// The size of Static mesh
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	float static_mesh_size = 0.3f;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	UStaticMesh* SelectedMesh1111111111111;
 
 	// 3////////////////////
-	
+
 	// Use TextRenderComponent or not
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool use_text_render_components_fornode = false;
@@ -233,19 +223,16 @@ public:
 	// The size of TextRenderComponent
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	float text_size = 10;
-	
+
 	UPROPERTY(VisibleAnywhere)
 	TArray<UTextRenderComponent*> TextComponents11111111111111111111;
 
 	// rotation to face the player
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool rotate_to_face_player = true;
-	
+
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
 
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -253,7 +240,7 @@ public:
 
 	// 1////////////////////
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
-	bool useactorforlink = false;	
+	bool useactorforlink = false;
 
 	// 2////////////////////
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
@@ -264,7 +251,7 @@ public:
 	// If want to use constant delta time
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	float use_constant_delta_time = 1;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	int32 maxiterations = 1000000;
 
@@ -274,19 +261,19 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	float tick_interval = 0.9f;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	int jnodes1 = 50;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool cpu_use_parallel = false;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool cpu_many_body_use_brute_force = true;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	bool connect_to_previous = true;
-	
+
 	float alpha = 1;
 	int iterationsf = 0;
 
@@ -295,7 +282,7 @@ public:
 
 	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Attributes)
 	float alphaDecay = 1 - FMath::Pow(alphaMin, 1.0 / 300);
-	
+
 	float edgeDistance = 30;
 	float nodeStrength = -60;
 	float distancemin = 1;
@@ -305,15 +292,15 @@ public:
 	float velocityDecay = 0.6;
 	float initialAngle = PI * (3 - sqrt(5));
 	float initialRadius = 10;
-	
 
-	
+
 	virtual void BeginPlay() override;
 	void gpugetpositions();
 	bool Maint(float DeltaTime);
 	virtual void BeginDestroy() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	virtual void Tick(float DeltaTime) override;
+
 	template <typename Func, typename... Args>
 	auto timeThisMemberFunction(const char* functionName, Func function, Args&&... args)
 	{
@@ -337,26 +324,16 @@ public:
 
 		return ElapsedTime;
 	}
+
 	void qq()
 	{
 		// This doesn't block the game thread. It just sent a request to end the game. 
-		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit, false);
+		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit,
+		                               false);
 	}
 
 	void s()
 	{
 		FNBodySimModule::Get().EndRendering();
 	}
-	
-	
 };
-
-
-
-
-
-
-
-
-
-
