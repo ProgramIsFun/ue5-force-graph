7c21fffdf31cac752c89eda6fdc427e2c70c50dd
Your Name <you@example.com>
2024-12-12 22:06:21 +0800

    a

--- Source/NBodySimulation/Pj11Character.cpp
+++ Source/NBodySimulation/Pj11Character.cpp
@@ -36,6 +36,12 @@ APj11Character::APj11Character()
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 
+
+
+	// We want to fly instead. 
+	// GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+
+	
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
