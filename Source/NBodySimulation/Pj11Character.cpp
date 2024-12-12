f231e93df60152c9208fdda0a396f38e951cc277
Your Name <you@example.com>
2024-12-12 19:05:32 +0800

    Update Pj11Character.cpp

--- Source/NBodySimulation/Pj11Character.cpp
+++ Source/NBodySimulation/Pj11Character.cpp
@@ -185,29 +185,29 @@ void APj11Character::LookUpAtRate2(float Rate)
 
 
 
-//
-// void APj11Character::Move(const FInputActionValue& Value)
-// {
-// 	// input is a Vector2D
-// 	FVector2D MovementVector = Value.Get<FVector2D>();
-//
-// 	if (Controller != nullptr)
-// 	{
-// 		// find out which way is forward
-// 		const FRotator Rotation = Controller->GetControlRotation();
-// 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-//
-// 		// get forward vector
-// 		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-// 	
-// 		// get right vector 
-// 		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-//
-// 		// add movement 
-// 		AddMovementInput(ForwardDirection, MovementVector.Y);
-// 		AddMovementInput(RightDirection, MovementVector.X);
-// 	}
-// }
+
+void APj11Character::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
 //
 // void APj11Character::Look(const FInputActionValue& Value)
 // {
